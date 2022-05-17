b99ed7052e965d923cd6263030c74db6b534ebe3
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-05-17 09:55:12 +0200

    sucursal and sucursalDTO

---
+++ S05T01N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n01/model/domain/Sucursal.java
@@ -0,0 +1,40 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n01.model.domain;
+
+public class Sucursal {
+    private Integer pk_SucursalID;
+    private String nomSucursal;
+    private String paisSucursal;
+
+    public Sucursal() {
+    }
+
+    public Sucursal(Integer pk_SucursalID, String nomSucursal, String paisSucursal) {
+        this.pk_SucursalID = pk_SucursalID;
+        this.nomSucursal = nomSucursal;
+        this.paisSucursal = paisSucursal;
+    }
+
+    public Integer getPk_SucursalID() {
+        return pk_SucursalID;
+    }
+
+    public void setPk_SucursalID(Integer pk_SucursalID) {
+        this.pk_SucursalID = pk_SucursalID;
+    }
+
+    public String getNomSucursal() {
+        return nomSucursal;
+    }
+
+    public void setNomSucursal(String nomSucursal) {
+        this.nomSucursal = nomSucursal;
+    }
+
+    public String getPaisSucursal() {
+        return paisSucursal;
+    }
+
+    public void setPaisSucursal(String paisSucursal) {
+        this.paisSucursal = paisSucursal;
+    }
+}
---
+++ S05T01N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s05/t01/n01/model/dto/SucursalDTO.java
@@ -0,0 +1,56 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s05.t01.n01.model.dto;
+
+public class SucursalDTO {
+    private Integer pk_SucursalID;
+    private String nomSucursal;
+    private String paisSucursal;
+    private String tipusSucursal;
+
+    public SucursalDTO() {
+    }
+
+    public SucursalDTO(Integer pk_SucursalID, String nomSucursal, String paisSucursal) {
+        this.pk_SucursalID = pk_SucursalID;
+        this.nomSucursal = nomSucursal;
+        this.paisSucursal = paisSucursal;
+    }
+
+    public SucursalDTO(Integer pk_SucursalID, String nomSucursal, String paisSucursal, String tipusSucursal) {
+        this.pk_SucursalID = pk_SucursalID;
+        this.nomSucursal = nomSucursal;
+        this.paisSucursal = paisSucursal;
+        this.tipusSucursal = tipusSucursal;
+    }
+
+    public Integer getPk_SucursalID() {
+        return pk_SucursalID;
+    }
+
+    public void setPk_SucursalID(Integer pk_SucursalID) {
+        this.pk_SucursalID = pk_SucursalID;
+    }
+
+    public String getNomSucursal() {
+        return nomSucursal;
+    }
+
+    public void setNomSucursal(String nomSucursal) {
+        this.nomSucursal = nomSucursal;
+    }
+
+    public String getPaisSucursal() {
+        return paisSucursal;
+    }
+
+    public void setPaisSucursal(String paisSucursal) {
+        this.paisSucursal = paisSucursal;
+    }
+
+    public String getTipusSucursal() {
+        return tipusSucursal;
+    }
+
+    public void setTipusSucursal(String tipusSucursal) {
+        this.tipusSucursal = tipusSucursal;
+    }
+}
