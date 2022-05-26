4710f8b7583cfae667e80bf41b2a66fa2ff64739
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-05-26 10:51:48 +0200

    S05T01N02SulzbachRilhoLucas - initial files

---
+++ S05T01N02SulzbachRilhoLucas/src/main/java/cat/itacademy/barcelonactiva/sulzbachrilho/lucas/s05/t01/n02/S05T01N02SulzbachRilhoLucas/S05T01N02SulzbachRilhoLucasApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.sulzbachrilho.lucas.s05.t01.n02.S05T01N02SulzbachRilhoLucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N02SulzbachRilhoLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N02SulzbachRilhoLucasApplication.class, args);
+	}
+
+}
---
+++ S05T01N02SulzbachRilhoLucas/src/test/java/cat/itacademy/barcelonactiva/sulzbachrilho/lucas/s05/t01/n02/S05T01N02SulzbachRilhoLucas/S05T01N02SulzbachRilhoLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.sulzbachrilho.lucas.s05.t01.n02.S05T01N02SulzbachRilhoLucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N02SulzbachRilhoLucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
