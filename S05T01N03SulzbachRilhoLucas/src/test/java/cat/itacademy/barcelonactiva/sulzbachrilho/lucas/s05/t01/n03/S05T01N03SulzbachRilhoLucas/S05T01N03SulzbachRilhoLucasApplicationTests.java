3000ffc8e7149fc9074779502579134d03f33d73
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-05-30 10:00:32 +0200

    S05T01N03 - initial files

---
+++ S05T01N03SulzbachRilhoLucas/src/test/java/cat/itacademy/barcelonactiva/sulzbachrilho/lucas/s05/t01/n03/S05T01N03SulzbachRilhoLucas/S05T01N03SulzbachRilhoLucasApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.sulzbachrilho.lucas.s05.t01.n03.S05T01N03SulzbachRilhoLucas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T01N03SulzbachRilhoLucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
